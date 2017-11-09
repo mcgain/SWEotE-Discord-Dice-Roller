3cacad3a420ec12eba460bec198e96b846215f60
Richard McGain <richard.mcgain@shopify.com>
2017-11-09 14:04:35 +1100

    firebase

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -1,7 +1,11 @@
 var config = {
-/***************
-PASTE FIREBASE CONFIG HERE
-***************/
+  // Initialize Firebase
+  apiKey: "AIzaSyDEETqKAk8-i8P1Mh9eSBg8Oz1JpOaXPZ8",
+  authDomain: "mcgain-starwars-diceroller.firebaseapp.com",
+  databaseURL: "https://mcgain-starwars-diceroller.firebaseio.com",
+  projectId: "mcgain-starwars-diceroller",
+  storageBucket: "mcgain-starwars-diceroller.appspot.com",
+  messagingSenderId: "116548653086"
 };
 
-  exports.config = config;
+exports.config = config;
